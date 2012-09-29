2c04536cc58b0cd01e1e310e04311c0398bc1c88
tearsofphoenix <tearsofphoenix@yahoo.cn>
2012-09-29 09:54:45 +0800

    focus on iOS.

--- LuaEngineCocoa/LuaEngineOSX.h
+++
@@ -1,9 +0,0 @@
-//
-//  LuaEngineOSX.h
-//  LuaEngineOSX
-//
-//  Created by tearsofphoenix tearsofphoenix on 6/23/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
-#import "LuaEngineService.h"
