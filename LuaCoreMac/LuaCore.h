9f29e4099bea781f78a182ce2a250d6bda2ee35d
Leix Snake <xiaoming.liu@eintsoft.com>
2012-10-31 16:12:45 +0800

    split lua into standalone framework.

---
+++ LuaCoreMac/LuaCore.h
@@ -0,0 +1,17 @@
+//
+//  LuaCoreMac.h
+//  LuaCoreMac
+//
+//  Created by LeixSnake on 10/31/12.
+//
+//
+
+#ifdef __cplusplus
+#import <LuaCore/lua.hpp>
+#else
+#import <LuaCore/lua.h>
+#endif
+
+#import <LuaCore/lauxlib.h>
+#import <LuaCore/luaconf.h>
+#import <LuaCore/lpeg.h>
