4f5ed36d6680768086c6a0d38bba4d084309c4c4
tearsofphoenix <tearsofphoenix@yahoo.cn>
2013-01-22 09:21:14 +0800

    move LuaKit here.

---
+++ LuaKit/LuaKit/LuaKit.h
@@ -0,0 +1,18 @@
+//
+//  LuaKit.h
+//  LuaKit
+//
+//  Created by tearsofphoenix on 12-11-2.
+//  Copyright (c) 2012年 tearsofphoenix. All rights reserved.
+//
+
+#ifdef __cplusplus
+#import <LuaKit/lua.hpp>
+#else
+#import <LuaKit/lua.h>
+#endif
+
+#import <LuaKit/lauxlib.h>
+#import <LuaKit/luaconf.h>
+#import <LuaKit/lpeg.h>
+#import <LuaKit/lualib.h>
