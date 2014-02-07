8ac95a5bb1dfd3694e001cdf10464ed0900a9727
tearsofphoenix <tearsofphoenix@icloud.com>
2014-02-07 17:39:16 +0800

    updated for lua 5.2.3, Xcode 5.

---
+++ LuaKit/LuaKitiOSTests/LuaKitiOSTests.m
@@ -0,0 +1,34 @@
+//
+//  LuaKitiOSTests.m
+//  LuaKitiOSTests
+//
+//  Created by Mac003 on 14-2-7.
+//  Copyright (c) 2014年 tearsofphoenix. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LuaKitiOSTests : XCTestCase
+
+@end
+
+@implementation LuaKitiOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
